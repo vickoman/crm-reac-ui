c986579b8b8ef6e4f3f9b9f0d7305665c89f652c
vickoman <vicmandlagasca@gmail.com>
2022-03-25 14:14:52 -0500

    Add tailwind cdn and Sidebar and Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Head from 'next/head';
+
+import Sidebar from '../components/Sidebar';
 
 const Layout = ({ children }) => {
     return (
-        <div>
-            <h1>Desde layout</h1>
-            {children}
-        </div>
+        <>
+            <Head>
+                <title>CRM - Admin clients</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <script src="https://cdn.tailwindcss.com"></script>
+            </Head>
+            <div className="bg-gray-200 min-h-screen">
+                <div className="flex min-h-screen">
+                    <Sidebar />
+                </div>
+                {children}
+            </div>
+        </>
     );
 };
 
