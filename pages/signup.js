af38c9f884bd8a88d39327eb707d4db7330446ff
vickoman <vicmandlagasca@gmail.com>
2022-03-26 12:55:27 -0500

    Add Login and signup

---
+++ pages/signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Signup = () => {
+    return (
+        <>
+            <Layout>
+                <h1>Signup</h1>
+            </Layout>
+        </>
+    )
+};
+
+export default Signup;
