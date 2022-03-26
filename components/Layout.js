c2d41e5eaedb7018d8d9746a5915b08951fd2c30
vickoman <vicmandlagasca@gmail.com>
2022-03-26 13:05:57 -0500

    Add validation to show sidebar only in internal pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import Sidebar from '../components/Sidebar';
 
 const Layout = ({ children }) => {
+    const router = useRouter();
     return (
         <>
             <Head>
@@ -13,14 +15,20 @@ const Layout = ({ children }) => {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <script src="https://cdn.tailwindcss.com"></script>
             </Head>
-            <div className="bg-gray-200 min-h-screen">
-                <div className="flex min-h-screen">
-                    <Sidebar />
-                    <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
-                        {children}
-                    </main>
+            { router.pathname === '/login' || router.pathname === '/signup' ? (
+                <div className="bg-gray-800 min-h-screen flex flex-col justify-center">
+                    {children}
                 </div>
-            </div>
+            ) : (
+                <div className="bg-gray-200 min-h-screen">
+                    <div className="flex min-h-screen">
+                        <Sidebar />
+                        <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+                            {children}
+                        </main>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
