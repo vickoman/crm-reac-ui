5cbdd1a86148e2e08664dac9d86a5cdfb216469b
vickoman <vicmandlagasca@gmail.com>
2022-03-26 13:18:12 -0500

    Add formik initial on signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup'; 
 
 const Signup = () => {
+
+    // Validate form
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            lastName: '',
+            email: '',
+            password: ''
+        },
+        onSubmit: values => {
+            console.log(values);
+        }
+    });
     return (
         <>
         <Layout>
             <h1 className="text-center text-2xl text-white font-light">Signup</h1>
             <div className="flex justify-center mt-5">
                 <div className="w-full max-w-xs">
-                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+                        onSubmit={formik.handleSubmit}
+                    >
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
                                 Name
@@ -16,7 +33,10 @@ const Signup = () => {
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                                 id="name"
                                 type="name"
-                                placeholder="Name" />
+                                placeholder="Name"
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
+                                 />
                         </div>
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
@@ -25,7 +45,10 @@ const Signup = () => {
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                                 id="lastName"
                                 type="lastName"
-                                placeholder="lastName" />
+                                placeholder="lastName"
+                                value={formik.values.lastName}
+                                onChange={formik.handleChange}
+                                 />
                         </div>
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -34,7 +57,10 @@ const Signup = () => {
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                                 id="email"
                                 type="email"
-                                placeholder="Email" />
+                                placeholder="Email"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                 />
                         </div>
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
@@ -43,7 +69,10 @@ const Signup = () => {
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                                 id="password"
                                 type="password"
-                                placeholder="User password" />
+                                placeholder="User password"
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                 />
                         </div>
                         <input
                             type="submit"
