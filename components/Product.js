15075f2ba4062c82bdf701eeb80d033037d0d505
vickoman <vicmandlagasca@gmail.com>
2022-03-30 02:35:25 -0500

    Enable update redirection

--- components/Product.js
+++ components/Product.js
@@ -66,14 +66,14 @@ const Product = ({product}) => {
           })
     }
 
-    // const editClient = (id) => {
-    //     Router.push({
-    //         pathname: '/clients/edit/[id]',
-    //         query: {
-    //             id
-    //         }
-    //     });
-    // };
+    const editProduct = (id) => {
+        Router.push({
+            pathname: '/products/edit/[id]',
+            query: {
+                id
+            }
+        });
+    };
 
     return (
         <tr key={id}>
@@ -94,7 +94,7 @@ const Product = ({product}) => {
                     <button
                         type="button"
                         className="flex justify-center bg-green-600 py-2 px-4 rounded text-white text-sm hover:bg-green-800 w-1/5 ml-3"
-                        onClick={() => console.log("should edit product")}
+                        onClick={() => editProduct(id)}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
