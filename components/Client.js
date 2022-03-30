95703399b7f2d3ac0c8b8bd19e50496e7df1ff52
vickoman <vicmandlagasca@gmail.com>
2022-03-30 01:47:16 -0500

    Fix Buttons delete and edit

--- components/Client.js
+++ components/Client.js
@@ -84,24 +84,22 @@ const Client = ({client}) => {
             <td className="border px-4 py-2">{company}</td>
             <td className="border px-4 py-2">{email}</td>
             <td className="border px-4 py-2">
-                <div className="flex">
+                <div className="flex justify-center">
                     <button
                         type="button"
-                        className="flex justify-center bg-red-800 py-2 px-4 rounded text-white text-sm hover:bg-red-900 w-full"
+                        className="flex justify-center bg-red-800 py-2 px-4 rounded text-white text-sm hover:bg-red-900 w-1/5"
                         onClick={() => confirmDeleteClient({id, company})}
                     >
-                        Delete 
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                         </svg>
                     </button>
                     <button
                         type="button"
-                        className="flex justify-center bg-green-600 py-2 px-4 rounded text-white text-sm hover:bg-green-800 w-full ml-3"
+                        className="flex justify-center bg-green-600 py-2 px-4 rounded text-white text-sm hover:bg-green-800 w-1/5 ml-3"
                         onClick={() => editClient(id)}
                     >
-                        Edit 
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                         </svg>
                     </button>
